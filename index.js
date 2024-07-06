d7c40b7908e485110c6207daf07b82f630253ba9
ianramy <imwagore@gmail.com>
2024-07-06 18:55:28 +0300

    origin/main

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+/**
+ * Returns the year the Denver Broncos won the Superbowl.
+ *
+ * @param {Array} record - An array of objects containing year and result properties.
+ * @return {string|undefined} - The year when the Broncos won the Superbowl or undefined if no win is found.
+ */
+function superbowlWin(record) {
+    for (let entry of record) {
+        if (entry.result === "W") {
+            return entry.year;
+        }
+    }
+    return undefined;
+}
+
+
